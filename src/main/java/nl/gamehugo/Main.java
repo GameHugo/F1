5da21f4e712205ec79c2b02065d02551360d1c73
GameHugo <85233079+GameHugo@users.noreply.github.com>
2024-07-27 22:37:04 +0200

    Add join listener and Mojang auth

--- src/main/java/nl/gamehugo/Main.java
+++ src/main/java/nl/gamehugo/Main.java
@@ -1,6 +1,11 @@
 package nl.gamehugo;
 
 import net.minestom.server.MinecraftServer;
+import net.minestom.server.coordinate.Pos;
+import net.minestom.server.entity.Player;
+import net.minestom.server.event.GlobalEventHandler;
+import net.minestom.server.event.player.AsyncPlayerConfigurationEvent;
+import net.minestom.server.extras.MojangAuth;
 import net.minestom.server.instance.InstanceContainer;
 import net.minestom.server.instance.InstanceManager;
 import net.minestom.server.instance.LightingChunk;
@@ -24,6 +29,16 @@ public class Main {
         // Set the ChunkGenerator
         instanceContainer.setGenerator(unit -> unit.modifier().fillHeight(0, 40, Block.GRASS_BLOCK));
         instanceContainer.setChunkSupplier(LightingChunk::new);
+
+        // Add an event callback to specify the spawning instance (and the spawn position)
+        GlobalEventHandler globalEventHandler = MinecraftServer.getGlobalEventHandler();
+        globalEventHandler.addListener(AsyncPlayerConfigurationEvent.class, event -> {
+            final Player player = event.getPlayer();
+            event.setSpawningInstance(instanceContainer);
+            player.setRespawnPoint(new Pos(0, 42, 0));
+        });
+
+        MojangAuth.init();
 
         // Start the server on port 25565
         minecraftServer.start("0.0.0.0", 25565);
